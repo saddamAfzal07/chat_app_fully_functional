d387ad974bd5488a5e58b9e1ff473330c58b5486
Saddam Afzal <84958454+saddamAfzal07@users.noreply.github.com>
2023-01-20 20:05:54 +0500

    chat

---
+++ lib/features/pages/chat_screen/chat_screen.dart
@@ -0,0 +1,271 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chat_app/api/apis.dart';
+import 'package:chat_app/models/message.dart';
+import 'package:chat_app/models/models.dart';
+import 'package:chat_app/widgets/message_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ChatScreen extends StatefulWidget {
+  final ChatUser user;
+  const ChatScreen({super.key, required this.user});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  //for storing all messages
+  List<Message> userChatList = [];
+  //for emoji
+  bool showEmoji = false;
+
+  final _textController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () {
+        if (showEmoji) {
+          setState(() {
+            showEmoji = !showEmoji;
+          });
+          return Future.value(false);
+        } else {
+          return Future.value(true);
+        }
+      },
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SafeArea(
+          child: Scaffold(
+            backgroundColor: Color.fromARGB(255, 179, 212, 240),
+            appBar: AppBar(
+              toolbarHeight: 65,
+              elevation: 2,
+              automaticallyImplyLeading: false,
+              flexibleSpace: _appbar(),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                      stream: Apis.getAllChats(widget.user),
+                      builder: (context, snapshot) {
+                        switch (snapshot.connectionState) {
+                          //if data is loading
+                          case ConnectionState.waiting:
+                          case ConnectionState.none:
+                            return Center(child: SizedBox());
+                          //if some and all data loaded
+                          case ConnectionState.active:
+                          case ConnectionState.done:
+                            // final data = snapshot.data?.docs;
+                            // print(snapshot.data!.docs.first);
+                            // userChatList
+                            //     .map((e) => Message.fromJson(e.data()))
+                            //     .toList();
+                            final data = snapshot.data!.docs;
+                            userChatList = data
+                                .map((e) => Message.fromJson(e.data()))
+                                .toList();
+
+                            print(userChatList);
+
+                            if (userChatList.isNotEmpty) {
+                              return ListView.builder(
+                                itemCount: userChatList.length,
+                                itemBuilder: (context, index) {
+                                  return MessageCard(
+                                    msg: userChatList[index],
+                                  );
+                                },
+                              );
+                            } else {
+                              return Center(
+                                  child: Text(
+                                "Say Hi..👋",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                ),
+                              ));
+                            }
+                        }
+                      },
+                    ),
+                  ),
+                  _textField(),
+                  if (showEmoji)
+                    SizedBox(
+                      height: MediaQuery.of(context).size.width * 0.6,
+                      child: EmojiPicker(
+                        textEditingController: _textController,
+                        config: Config(
+                          columns: 8,
+                          emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
+                        ),
+                      ),
+                    )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textField() {
+    return Row(
+      children: [
+        Expanded(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                      setState(() {
+                        showEmoji = !showEmoji;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.emoji_emotions,
+                      color: Colors.blue,
+                      size: 26,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      onTap: () {
+                        if (showEmoji)
+                          setState(() {
+                            showEmoji = !showEmoji;
+                          });
+                      },
+                      controller: _textController,
+                      textInputAction: TextInputAction.newline,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Type something...",
+                        hintStyle: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      final ImagePicker picker = ImagePicker();
+                      // Pick an image
+                      final XFile? image = await picker.pickImage(
+                          source: ImageSource.gallery, imageQuality: 80);
+                      if (image != null) {
+                        print(image.path);
+
+                        Apis.sendChatImage(widget.user, File(image.path));
+                      }
+                    },
+                    icon: Icon(
+                      Icons.photo,
+                      color: Colors.blue,
+                      size: 26,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      final ImagePicker picker = ImagePicker();
+                      // Pick an image
+                      final XFile? image = await picker.pickImage(
+                          source: ImageSource.camera, imageQuality: 80);
+                      if (image != null) {
+                        print(image.path);
+
+                        Apis.sendChatImage(widget.user, File(image.path));
+                      }
+                    },
+                    icon: Icon(
+                      Icons.camera_alt_outlined,
+                      color: Colors.blue,
+                      size: 26,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 5),
+        MaterialButton(
+          minWidth: 0,
+          elevation: 5,
+          padding: EdgeInsets.only(
+            left: 15,
+            right: 10,
+            top: 10,
+            bottom: 10,
+          ),
+          color: Colors.green,
+          shape: CircleBorder(),
+          onPressed: () {
+            if (_textController.text.isNotEmpty) {
+              Apis.sendMessage(widget.user, _textController.text, Type.text);
+              _textController.clear();
+            }
+          },
+          child: Icon(
+            Icons.send,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _appbar() {
+    return ListTile(
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: CachedNetworkImage(
+          height: 50,
+          width: 50,
+          imageUrl: widget.user.image == ""
+              ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+              : widget.user.image.toString(),
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
+      ),
+      title: Text(
+        widget.user.name.toString(),
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      subtitle: Text(
+        "not available",
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
+  }
+}
